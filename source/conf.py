7562fdc16162c08086c41a8e650a08b1412892d4
Simon Charette <simon.charette@emailicious.com>
2015-11-24 00:52:50 -0500

    Avoid using the Python intersphinx.

--- source/conf.py
+++ source/conf.py
@@ -364,7 +364,7 @@ epub_exclude_files = ['search.html']
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {}
 
 locale_dirs = ['locale/']
 gettext_compact = True
