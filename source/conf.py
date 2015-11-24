135d00b540b80ab93914c58f906e056441add945
Simon Charette <simon.charette@emailicious.com>
2015-11-24 00:45:26 -0500

    Added a French translation.

--- source/conf.py
+++ source/conf.py
@@ -365,3 +365,6 @@ epub_exclude_files = ['search.html']
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
+
+locale_dirs = ['locale/']
+gettext_compact = True
